8d7af66f19fef3f36d05036f5275e0d6cac6570b
AbDulla Musa <abdullamusa@hotmail.com>
2023-07-18 14:09:27 +0300

    edit for bahaa

--- routes/V1.0/general.php
+++ routes/V1.0/general.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function(){
     Route::get('getAllGrades', [GradeController::class, 'getAllGrades']);
-    Route::get('getAllClassesAndSubjectsOfGrade/{grade}', [GradeController::class, 'getAllClassesAndSubjects']);
+    Route::get('getAllGradesWithClassesAndSubjects', [GradeController::class, 'getAllGradesWithClassesAndSubjects']);
 });
