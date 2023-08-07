f980bab6c6da936b4b7650f9638132f3145d9dde
AbdullahRahmoun <aboodyggkol@gmail.com>
2023-08-08 02:42:39 +0300

    Small changes

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home.html');
 });
 Route::get('fuck',[NumberController::class,'khara'])
 ->middleware('auth:sanctum');
