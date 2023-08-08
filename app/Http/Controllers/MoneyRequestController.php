67683dc28f7580a0d42ba0e61e51c6c16a9626c4
AbdullahRahmoun <aboodyggkol@gmail.com>
2023-08-08 20:30:55 +0300

    Small changes

--- app/Http/Controllers/MoneyRequestController.php
+++ app/Http/Controllers/MoneyRequestController.php
@@ -11,7 +11,7 @@ class MoneyRequestController extends Controller
         $data=request()->validate([
             'totalValue'=>['required','min:1000','numeric'],
             'payments'=>['required','array','min:1'],
-            'payments.*.value'=>['required','numeric','between:1000,'.$request->totalValue],
+            'payments.*.value'=>['required','numeric','between:1000,'.request()->totalValue],
             'payments.*.deadline_date'=>['']
         ]);
     }
