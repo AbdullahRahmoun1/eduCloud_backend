46dbf0e6ce56ca5d5a7cb275301265942764a87f
AbdullahRahmoun <aboodyggkol@gmail.com>
2023-08-12 21:35:27 +0300

    Small changes

--- routes/channels.php
+++ routes/channels.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
-Broadcast::channel('private-student-51',fn()=>true);
+Broadcast::channel('private-student-1',fn()=>true);
 Broadcast::channel(Helper::getStudentChannel("{student_id}")
 ,fn($student_id)=>true
 );
